576f67289003741a6684123bae20499b0516bda2
Eliot Fredman <eliot.fredman@gmail.com>
2025-05-17 01:38:03 +0300

    Major changes to `Character` - Character now has `getResistance()` and `getWeakness()` functions that   return and optional double if found in the map - Added `[[nodiscard]]` to resistances and weaknesses getters - Updated `updateStats()` to handle the optional data - Other changes

--- src/core/character_base.cpp
+++ src/core/character_base.cpp
@@ -1,31 +1,36 @@
 #include "core/character_base.hpp"
+#include <unordered_map>
 
-Character::Character(Attributes attrs, std::map<Element, float> res, std::map<Element, float> weak)
-	: attributes(attrs), resistances(res), weaknesses(weak)
+using std::unordered_map;
+
+Character::Character(const Attributes& attrs, const unordered_map<Element, double>& res,
+                     const std::unordered_map<Element, double>& weak)
+	 : attributes(attrs), stats(), bonus(),
+		resistances_ptr(std::make_unique<unordered_map<Element, double>>(res)),
+		weaknesses_ptr(std::make_unique<unordered_map<Element, double>>(weak))
 {
 	updateStats();
 }
 
 void Character::updateStats()
 {
-	stats.health  = (int)(attributes.vitality * 0.8f + attributes.endurance * 0.4f) * 2;
-	stats.stamina = (int)(attributes.vitality * 0.4f + attributes.endurance * 0.8f) * 2;
-	stats.speed   = (int)(attributes.vitality * 0.3f + attributes.dexterity * 0.7f);
-	stats.mana    = (int)(attributes.willpower + attributes.wisdom * 0.4f);
+	stats.health  = static_cast<int>((attributes.vitality * 0.8 + attributes.endurance * 0.4) * 2);
+	stats.stamina = static_cast<int>((attributes.vitality * 0.4 + attributes.endurance * 0.8) * 2);
+	stats.speed   = static_cast<int>(attributes.vitality * 0.3 + attributes.dexterity * 0.7);
+	stats.mana    = static_cast<int>((attributes.willpower + attributes.wisdom * 0.4) * 4);
 	stats.range   = (int)(attributes.size);
 	stats.defense = attributes.endurance;
 }
 
 void Character::takeDamage(int damage, const Element& damage_type)
 {
-	damage *= 1.0f - resistances[Element::ALL] + weaknesses[Element::ALL]
-	        - resistances[damage_type] + weaknesses[damage_type];
-	// damage = damage < 0 ? 0 : damage;
+	damage *= 1 - getResistance(Element::ALL).value_or(0.0) + getWeakness(Element::ALL).value_or(0.0)
+	        - getResistance(damage_type).value_or(0.0) + getWeakness(damage_type).value_or(0.0);
+	damage = damage < 0 ? 0 : damage;
 	stats.health -= damage;
 }
 
 Character::Attributes::Attributes(int vi, int en, int st, int de, int ch, int in, int wi, int wp, Size si)
-	: vitality(vi), endurance(en), strength(st), dexterity(de), charisma(ch), intelligence(in),
-	  wisdom(wi), willpower(wp), size(si)
-{
-}
+	 : vitality(vi), endurance(en), strength(st), dexterity(de), charisma(ch), intelligence(in),
+		wisdom(wi), willpower(wp), size(si)
+{}
--- src/core/character_base.hpp
+++ src/core/character_base.hpp
@@ -1,18 +1,22 @@
 #pragma once
 
 #include "combat/elements.hpp"
-#include <map>
+#include <cassert>
+#include <initializer_list>
 #include <memory>
+#include <optional>
+#include <unordered_map>
 
-using std::map;
 
 /// Size classes of characters
-enum class Size { TINY = 0, SMALL = 1, MEDIUM = 1, LARGE = 2, HUGE = 3 };
+enum class Size : char { TINY = 0, SMALL = 1, MEDIUM = 1, LARGE = 2, HUGE = 3 };
 
 /// Base class for all characters (e.g. enemies, NPCs, player)
 class Character
 {
-  public:
+	using ElementMap = std::unordered_map<Element, double>;
+	using Element::ALL;
+
 	/// Contains attributes of character
 	struct Attributes
 	{
@@ -20,27 +24,31 @@ class Character
 		int  charisma, intelligence, wisdom, willpower; // Mental
 		Size size = Size::MEDIUM;
 
-	  public:
+	 public:
 		Attributes(int vitality, int endurance, int strength, int dexterity, int charisma,
 		           int intelligence, int wisdom, int willpower, Size size = Size::MEDIUM);
 	};
 
 	struct Stats
 	{
-		int health, stamina, mana, range, speed, defense;
+		int health{}, stamina{}, mana{}, range{}, speed{}, defense{};
 	};
 
-  protected:
+
+ protected:
 	Attributes attributes;
 	Stats      stats, bonus;
 
-	std::unique_ptr<map<Element, float>> resistances_ptr = std::make_unique<map<Element, float>>(); ///< Value is resistance coefficient between 0.0-1.0
-	std::unique_ptr<map<Element, float>> weaknesses_ptr = std::make_unique<map<Element, float>>(); ///< Value is weakness coefficient between 0.0-1.0
-	map<Element, float>& resistances = *resistances_ptr;
-	map<Element, float>& weaknesses  = *weaknesses_ptr;
+	std::unique_ptr<ElementMap> resistances_ptr; ///< Value is resistance coefficient between 0.0-1.0
+	std::unique_ptr<ElementMap> weaknesses_ptr;  ///< Value is weakness coefficient between 0.0-1.0
 
-  public:
-	Character(Attributes attributes, map<Element, float> resistances, map<Element, float> weaknesses);
+ public:
+	Character(const Character&)            = delete;
+	Character(Character&&)                 = default;
+	Character& operator=(const Character&) = delete;
+	Character& operator=(Character&&)      = default;
+	Character(const Attributes& attributes, const ElementMap& resistances, const ElementMap& weaknesses);
+	virtual ~Character() = default;
 
 	/// Reduce health by damage - defense and apply relevant resistances and weaknesses
 	virtual void takeDamage(int damage, const Element& damage_type);
@@ -48,7 +56,20 @@ class Character
 	/// Initialize stats for character
 	virtual void updateStats();
 
-	const map<Element, float>& getResitances() const { return resistances; }
+	void setResitances(Element element, double value) { resistances_ptr->emplace(element, value); }
 
-	const map<Element, float>& getWeaknesses() const { return weaknesses; }
+	[[nodiscard]] ElementMap& getResistances() { return *resistances_ptr; }
+	[[nodiscard]] ElementMap& getWeaknesses() { return *weaknesses_ptr; }
+
+	[[nodiscard]] std::optional<double> getResistance(Element element) const
+	{
+		const auto& it = resistances_ptr->find(element);
+		return it != resistances_ptr->end() ? it->second : std::optional<double>{};
+	}
+
+	[[nodiscard]] std::optional<double> getWeakness(Element element) const
+	{
+		const auto& it = weaknesses_ptr->find(element);
+		return it != weaknesses_ptr->end() ? it->second : std::optional<double>{};
+	}
 };

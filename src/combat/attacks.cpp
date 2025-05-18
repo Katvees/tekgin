12d67c1e72f0e0450f2e57912e3b3f7dc9c6810b
Eliot Fredman <eliot.fredman@gmail.com>
2025-05-19 00:10:50 +0300

    Attacks now require a function to be passed for damage calculation
    Addition: `Attack::damage_solver` Function pointer to function that calculates attack damage
    	Note: This method allows any object to own the attack and lambdas can be used to capture owner or its variables
    Addition: Added some comments
    
    Change: Removed pointer to `Character*`
    	Note: It wasn't safe as ownership was not enforced and unneeded with the new implementation of `Attack::calculateDamage()`
    Change: `Attack::calculateDamage()` implementation now either runs `Attack::damage_solver` or returns 0

--- src/combat/attacks.cpp
+++ src/combat/attacks.cpp
@@ -1,18 +1,10 @@
 #include "combat/attacks.hpp"
-#include "core/character_base.hpp"
 
-/// Temporary implementation! Will change!
 int Attack::calculateDamage() const
 {
-	const Character::Attributes& attributes = attacker->getAttributes();
-	switch (attack_type) {
-	case Kind::MELEE:
-		return attributes.strength * damage_coeff;
-		break;
-	case Kind::RANGED:
-		return (attributes.strength * 0.6 + attributes.dexterity * 0.4) * damage_coeff;
-		break;
-	case Kind::MAGIC:
-		return attributes.willpower * damage_coeff;
+	// Check for damage solver
+	if (damage_solver) {
+		return damage_solver(*this);
 	}
+	return 0;
 }
--- src/combat/attacks.hpp
+++ src/combat/attacks.hpp
@@ -1,14 +1,17 @@
 #pragma once
 
 #include "combat/elements.hpp"
-#include "core/character_base.hpp"
+#include <functional>
 
+/// Implemented as an aggregate struct for easier definition as all fields aren't usually needed
 struct Attack
 {
 	enum class Kind : char { MELEE, RANGED, MAGIC };
 	double  damage_coeff = 1.0, range_coeff = 1.0, speed_coeff = 1.0;
 	Kind    attack_type = Kind::MELEE;
 	Element damage_type = Element::NONE;
-	Character* attacker = nullptr;
+	/// A function (usually lambda) that calculates the damage for the attack
+	std::function<int(const Attack&)> damage_solver;
+	/// Calculate damage of attack using damage_sovler if set
 	[[nodiscard]] int calculateDamage() const;
 };

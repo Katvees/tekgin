386c419c9c0bf3bfba8987a3a04107fe4f394ed1
Eliot Fredman <eliot.fredman@gmail.com>
2025-05-20 01:25:25 +0300

    Fixed typos and modified used language Change: Changed documentation comment to normal comment Change: Removed unnecessary information from comment Change: Fixed typo

--- src/combat/attacks.hpp
+++ src/combat/attacks.hpp
@@ -5,16 +5,16 @@
 
 namespace Tekgin
 {
-	/// Implemented as an aggregate struct for easier definition as all fields aren't usually needed
+	// Implemented as an aggregate struct for easier definition as all fields aren't usually needed
 	struct Attack
 	{
 		enum class Kind : char { MELEE, RANGED, MAGIC };
 		double  damage_coeff = 1.0, range_coeff = 1.0, speed_coeff = 1.0;
 		Kind    attack_type = Kind::MELEE;
 		Element damage_type = Element::NONE;
-		/// A function (usually lambda) that calculates the damage for the attack
+		/// A function that calculates the damage for the attack
 		std::function<int(const Attack&)> damage_solver;
-		/// Calculate damage of attack using damage_sovler if set
+		/// Calculate damage of attack using damage_solver if set
 		[[nodiscard]] int calculateDamage() const;
 	};
 } // namespace Tekgin

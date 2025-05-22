f4304be3e2e6470e14be398c5525565662cfbc7e
Eliot Fredman <eliot.fredman@gmail.com>
2025-05-23 00:40:35 +0300

    Declared "interface" for saveable classes Addition: Declared interface `Saveable`, which declares all children will have a UUID and can save and load to file

--- src/core/interface/saveable.hpp
+++ src/core/interface/saveable.hpp
@@ -1,14 +1,26 @@
 #pragma once
+
+#include "core/character_base.hpp"
+#include <optional>
 #include <uuid/uuid.h>
 
 namespace Tekgin
 {
 struct ISaveable
 {
-	virtual ~ISaveable() = default;
-	virtual void Save()  = 0;
-	virtual void Load()  = 0;
+	ISaveable()                            = delete;
+	ISaveable(const ISaveable&)            = delete;
+	ISaveable& operator=(const ISaveable&) = delete;
+	ISaveable(ISaveable&&)                 = delete;
+	ISaveable& operator=(ISaveable&&)      = delete;
+	virtual ~ISaveable()                   = 0;
 
-	static void IsLoaded(const uuid_t uuid) = {};
+	virtual void Save() = 0;
+	virtual void Load() = 0;
+
+	uuid_t uuid{};
+
+	template<Entity E>
+	static std::optional<E> IsLoaded(const uuid_t uuid);
 };
 } // namespace Tekgin

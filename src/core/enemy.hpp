c3f9301803f9285b7941a57a02c10465e95ced6c
Eliot Fredman <eliot.fredman@gmail.com>
2025-05-23 03:21:54 +0300

    `Enemy` is now derived from `ISaveable` Addition: Added `ISaveable` to base classes of `Enemy` Addition: Declared `lave()` and `load()` functions for `Enemy` Addition: Implemented inline getters for id and id hash for `Enemy`

--- src/core/enemy.hpp
+++ src/core/enemy.hpp
@@ -1,8 +1,26 @@
 #pragma once
 #include "core/character_base.hpp"
+#include "core/interface/saveable.hpp"
+#include <functional>
+
+#define ulong unsigned long long
+
+using std::hash;
 
 namespace Tekgin
 {
-	class Enemy : public Character
-	{};
+class Enemy : public Character, ISaveable
+{
+	static ulong count;
+
+	ulong       id;
+	hash<ulong> id_hash;
+
+ public:
+	void Save() override;
+	void Load() override;
+
+	ulong       getId() override { return id; };
+	hash<ulong> getIdHash() override { return id_hash; };
+};
 } // namespace Tekgin

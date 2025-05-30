ea49b9fa027090c84e4878d9864952cf84582ae5
Eliot Fredman <eliot.fredman@gmail.com>
2025-05-30 04:28:00 +0300

    Moved documentation comment to be in the correct position Change: Moved documentation comment of `Elements` to be next to it         instead of outside the namespace

---
+++ include/tekgin/combat/elements.hpp
@@ -0,0 +1,8 @@
+#pragma once
+// Consider moving to another file to reduce filecount and avoid nearly empty files
+
+namespace Tekgin
+{
+/// Elements for damage types etc.
+enum class Element : char { NONE, ALL, FIRE, COLD, LIGHTNING, POISON };
+}

ffbdd1394487945b47801d88abceb32809fded10
Eliot Fredman <eliot.fredman@gmail.com>
2025-06-07 22:15:21 +0300

    Made CMakeLists work with new modularized model Change: Modified CMakeLists.txt to work with modules Change: Modified build script to work better with modules 	Details: Removed include linking and other now redundant code

--- build.sh
+++ build.sh
@@ -11,17 +11,3 @@ if [[ ${CLEAN,,} == "true" ]]; then
 fi
 cmake -S . -G "$GENERATOR" -B build $BUILD_ARGS
 cmake --build build
-
-root_dir="$(pwd)"
-include_dir="$root_dir/build/tekgin/include" 
-
-header_paths="$(find "$root_dir"/src -name "*.[h]pp" -type f)"
-
-[[ ${CLEAN,,} != "true" ]] && trash "$include_dir" -r
-mkdir "$include_dir" -p
-ln -s . "$include_dir/tekgin"
-for header_path in $header_paths; do
-	new_header="${header_path/src/build\/tekgin\/include}"
-	mkdir "${new_header%/*}" -p
-	cp "$header_path" "$new_header"
-done

8196eeb2e1c1139765bf09dcd795d8ce52da796b
Eliot Fredman <eliot.fredman@gmail.com>
2025-06-18 04:44:25 +0300

    Modified .clangd and .clang-format Change: Modified format rules a bit Addition: Added compiler to .clangd Addition: Made build script auto format all files

--- build.sh
+++ build.sh
@@ -5,9 +5,17 @@ for arg in "$@"; do
 	eval "${arg%%=*}"=\'"${arg#*=}"\'
 done
 
-if [[ ${CLEAN,,} == "true" ]]; then
+if [[ ${NO_FORMAT,,} == "true" ]] || [[ ${NO_FORMAT,,} == "yes" ]] || [[ ${NO_FORMAT,,} == "on" ]]; then
+	echo "Skipped formatting"
+else
+	echo "Formatting files.."
+	clang-format src/**/*.cpp{,m} -i
+fi
+
+if [[ ${CLEAN,,} == "true" ]] || [[ ${CLEAN,,} == "yes" ]] || [[ ${CLEAN,,} == "on" ]]; then
 	echo "Cleaning build directory..."
 	rm -rf build/{,.}*
 fi
+
 cmake -S . -G "$GENERATOR" -B build $BUILD_ARGS
 cmake --build build
